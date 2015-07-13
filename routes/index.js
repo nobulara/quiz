25abab8da17798ea843387a45cb5288b6613a704
nobulara <nobulara@polonorte.es>
2015-07-13 22:19:10 +0200

    Añadidos cambios para mostrar un enlace con el autor y su foto

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author');	
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
