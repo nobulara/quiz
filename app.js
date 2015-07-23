b0baa696134f6eda723a605fae745deff8621c60
nobulara <nobulara@polonorte.es>
2015-07-23 18:58:48 +0200

    Despliegue DB en Heroku

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(partials());
 
 // uncomment after placing your favicon in /public
