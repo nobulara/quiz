476c252ab613cb3f613a7701e95c059d0d221122
nobulara <nobulara@polonorte.es>
2015-08-01 19:31:41 +0200

    Añadido borrado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -109,3 +109,10 @@ exports.update = function(req, res) {
 		}
 	});		
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
